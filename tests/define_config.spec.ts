aebc8f96cece40749e2b61589384f6f04c38f99c
Lucas Finoti <42899930+FinotiLucas@users.noreply.github.com>
2025-01-22 12:23:41 -0300

    update

--- tests/define_config.spec.ts
+++ tests/define_config.spec.ts
@@ -4,11 +4,20 @@ import app from '@adonisjs/core/services/app'
 
 test.group('Config', () => {
   test('it should be able to make the package config', async ({ assert }) => {
-    const issuer = 'My App'
+    const issuer = 'My app'
+    const numberOfSecretBytes = 20
+    const recoveryCodesLength = 16
+    const recoveryCodeSize = 10
 
-    const config = await defineConfig({ issuer }).resolver(app)
+    const config = await defineConfig({
+      issuer,
+      numberOfSecretBytes,
+      recoveryCodeSize,
+      recoveryCodesLength,
+     }).resolver(app)
 
     assert.properties(config, ['issuer'])
     assert.equal(config.issuer, issuer)
+
   })
 })
